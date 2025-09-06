e352709a3431a42c12619d162fcfca3478e760ef
rexchoppers <connorbrady@codox.uk>
2025-09-06 17:29:39 +0100

    UPDATED Script to pull from methods/

--- generate.js
+++ generate.js
@@ -3,11 +3,23 @@ const fs = require("fs");
 const registerClient = require("./methods/registerClient.json");
 const info = require("./info.json");
 
+const methods = [];
+
+// Iterate through methods, add to methods array
+const methodsDir = './methods';
+fs.readdirSync(methodsDir).forEach(file => {
+    if (file.endsWith('.json')) {
+        const method = require(`./methods/${file}`);
+        methods.push(method);
+    }
+});
+
 const spec = {
     openrpc: "1.3.2",
     info,
-    methods: [registerClient],
+    methods,
 };
 
 fs.writeFileSync("openrpc.build.json", JSON.stringify(spec, null, 2));
 console.log("✅ Built openrpc.build.json");
+
