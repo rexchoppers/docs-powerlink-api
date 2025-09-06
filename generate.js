2cd92ba99ab3f1eabfebfc20bb78a29f378e725c
rexchoppers <connorbrady@codox.uk>
2025-09-06 15:27:28 +0100

    WIP Sorting main build information

---
+++ generate.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const registerClient = require("./methods/registerClient.json");
+
+const spec = {
+    openrpc: "1.3.2",
+    info: {},
+    methods: [registerClient],
+};
+
+fs.writeFileSync("openrpc.build.json", JSON.stringify(spec, null, 2));
+console.log("✅ Built openrpc.build.json");
